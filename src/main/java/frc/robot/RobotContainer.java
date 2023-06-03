d351279b829ef6462bd782e74fa8b4dd8b5004fb
uncoolnehuel <74217788+uncoolnehuel@users.noreply.github.com>
2023-06-03 09:44:31 -0400

    Manually added in auto modes from DCMP
    Renamed a bit
    BC means Battlecry auto
    For naming:
    Down is towards cablebump
    Up is away (look at pathplanner map)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -58,9 +58,9 @@ public class RobotContainer {
 
   static enum AUTOS {
     EMPTY, SIX_TAXI, SEVEN_CHARGE, SIX_CONE_TAXI, CONE_TAXI_CHARGE,
-    CONE_PICKUP_CONE, CUBE_MID_TAXI_CHARGE, CONE_TAXI_EIGHT, CONE_PICKUP_CONE_EIGHT, CUBE_HIGH_CHARGE,
-    Seven_CubeH_Foxtrot_Cone_Engage, Six_CubeH_Echo_Cone_6_4, Seven_CubeH_Golf_Cone_Engage, Eight_CubeH_Hotel_Cone_8_4,
-    TEST};
+    CONE_PICKUP_CONE, BC_CUBE_MID_TAXI_CHARGE, CONE_TAXI_EIGHT, CONE_PICKUP_CONE_EIGHT, BC_CUBE_HIGH_DOWN_CHARGE, CUBE_HIGH_CHARGE,
+    BC_Seven_CubeH_Foxtrot_Cone_Engage, Six_CubeH_Echo_Cone_6_4, BC_Seven_CubeH_Golf_Cone_Engage, Eight_CubeH_Hotel_Cone_8_4,
+    TEST}
   public SendableChooser<AUTOS> firstAutoCmd = new SendableChooser<>();
   // private SendableChooser<Command> secondAutoCmd = new SendableChooser();
   // private SendableChooser<Command> thirdAutoCmd = new SendableChooser();
@@ -70,8 +70,6 @@ public class RobotContainer {
     m_arm = new Arm();
     m_limeLight = new LimeLight();
     m_driveController = new XboxController(Constants.DRIVE_CONTROLLER);
-
-    //m_IntakeV1_Lentz = new IntakeV1_Lentz();
 
     m_Dave_Intake = new Dave_Intake();
     m_blinkin = new RevBlinkin(m_Dave_Intake);
@@ -145,7 +143,7 @@ public class RobotContainer {
     PathPlannerServer.startServer(5811);
 
    // firstAutoCmd.setDefaultOption("Empty", AUTOS.EMPTY);
-    firstAutoCmd.setDefaultOption("Cube High Charge", AUTOS.CUBE_HIGH_CHARGE);
+    firstAutoCmd.setDefaultOption("CubeH Charge", AUTOS.CUBE_HIGH_CHARGE);
     firstAutoCmd.addOption("Taxi (Sides)", AUTOS.SIX_TAXI);
     firstAutoCmd.addOption("Charge (Station)", AUTOS.SEVEN_CHARGE);
     firstAutoCmd.addOption("Cone Taxi (Sides)", AUTOS.SIX_CONE_TAXI);
@@ -153,12 +151,13 @@ public class RobotContainer {
     firstAutoCmd.addOption("ConePickupCone (6)", AUTOS.CONE_PICKUP_CONE);
     firstAutoCmd.addOption("Cone Taxi (8)", AUTOS.CONE_TAXI_EIGHT);
     firstAutoCmd.addOption("Cone Pickup Cone (8)", AUTOS.CONE_PICKUP_CONE_EIGHT);
-    firstAutoCmd.addOption("Mid Cube Taxi Charge (Station)", AUTOS.CUBE_MID_TAXI_CHARGE);
-    firstAutoCmd.addOption("Test", AUTOS.TEST);
-    firstAutoCmd.addOption("CubeH Foxtrot/Charlie Cone Engage (Station)", AUTOS.Seven_CubeH_Foxtrot_Cone_Engage);
+    firstAutoCmd.addOption("[BC] Mid Cube Taxi Charge (Station)", AUTOS.BC_CUBE_MID_TAXI_CHARGE);
+    firstAutoCmd.addOption("[BC] CubeH (Up)Foxtrot/Charlie Cone Engage (Station)", AUTOS.BC_Seven_CubeH_Foxtrot_Cone_Engage);
     firstAutoCmd.addOption("CubeH Echo/Delta Cone 6.4 (6)", AUTOS.Six_CubeH_Echo_Cone_6_4);
-    firstAutoCmd.addOption("CubeH Golf/Bravo Cone Engage (Station)", AUTOS.Seven_CubeH_Golf_Cone_Engage);
-    firstAutoCmd.addOption("CubeH Hotel/Alph Cone 8.4 (8)", AUTOS.Eight_CubeH_Hotel_Cone_8_4);
+    firstAutoCmd.addOption("[BC] CubeH (Down)Golf/Bravo Cone Engage (Station)", AUTOS.BC_Seven_CubeH_Golf_Cone_Engage);
+    firstAutoCmd.addOption("CubeH Hotel/Alpha Cone 8.4 (8)", AUTOS.Eight_CubeH_Hotel_Cone_8_4);
+    firstAutoCmd.addOption("[BC] CubeH Charge", AUTOS.BC_CUBE_HIGH_DOWN_CHARGE);
+    firstAutoCmd.addOption("Test", AUTOS.TEST);
 
     SmartDashboard.putData("Auto Selector", firstAutoCmd);
     // #endregion
@@ -343,7 +342,7 @@ public class RobotContainer {
         );
       break; }
 
-      case CUBE_MID_TAXI_CHARGE: {
+      case BC_CUBE_MID_TAXI_CHARGE: {
       autoCommand = new SequentialCommandGroup(
         new InstantCommand(() -> m_chassis.resetPose(new Pose2d(0, 0, Rotation2d.fromDegrees(180)))),
         new SetIntake(m_Dave_Intake, DoubleSolenoid.Value.kReverse, Constants.CUBE_HOLD_SPEED),
@@ -352,13 +351,11 @@ public class RobotContainer {
         new WaitCommand(0.4),
         new SetIntake(m_Dave_Intake, DoubleSolenoid.Value.kReverse, Constants.CUBE_HOLD_SPEED),
         new SetArm(m_arm, Constants.ELBOW_STOW, Constants.SHOULDER_STOW),
-        new TraverseChargeStation(m_chassis, Constants.CHARGE_SPEED, Constants.EXTRA_TIME),
-        new WaitCommand(0.5),
-        new RideAutoChargeStation(m_chassis, -Constants.CHARGE_SPEED),
+        new AutoChargeStation(m_chassis, Constants.CHARGE_SPEED),
         new Park(m_chassis)
       );
       break; }
-      case Seven_CubeH_Foxtrot_Cone_Engage: {
+      case BC_Seven_CubeH_Foxtrot_Cone_Engage: {
         PathPlannerTrajectory seven_CubeH_Foxtrot_Cone_E_P1 = PathFunctions.createTrajectory("7_CubeH_Foxtrot_Cone_E_P1");
         PathPlannerTrajectory seven_CubeH_Foxtrot_Cone_E_P2 = PathFunctions.createTrajectory("7_CubeH_Foxtrot_Cone_E_P2");
         PathPlannerTrajectory seven_CubeH_Foxtrot_Cone_E_P3 = PathFunctions.createTrajectory("7_CubeH_Foxtrot_Cone_E_P3");
@@ -393,7 +390,7 @@ public class RobotContainer {
       );
       break; }
       
-      case Seven_CubeH_Golf_Cone_Engage: {
+      case BC_Seven_CubeH_Golf_Cone_Engage: {
         PathPlannerTrajectory seven_CubeH_Golf_Cone_E_P1 = PathFunctions.createTrajectory("7_CubeH_Golf_Cone_E_P1");
         PathPlannerTrajectory seven_CubeH_Golf_Cone_E_P2 = PathFunctions.createTrajectory("7_CubeH_Golf_Cone_E_P2");
         PathPlannerTrajectory seven_CubeH_Golf_Cone_E_P3 = PathFunctions.createTrajectory("7_CubeH_Golf_Cone_E_P3");
@@ -481,7 +478,7 @@ public class RobotContainer {
         break;
       }
 
-      case CUBE_HIGH_CHARGE: {
+      case BC_CUBE_HIGH_DOWN_CHARGE: {
         PathPlannerTrajectory seven_DownCharge = PathFunctions.createTrajectory("7_DownCharge");
         autoCommand = new SequentialCommandGroup(
           PathFunctions.resetOdometry(m_chassis, seven_DownCharge),
@@ -495,7 +492,21 @@ public class RobotContainer {
           new AutoChargeStation(m_chassis, Constants.CHARGE_SPEED),
           new Park(m_chassis)
         );
-        break; }
+      break; }
+
+      case CUBE_HIGH_CHARGE: {
+        autoCommand = new SequentialCommandGroup(
+          new InstantCommand(() -> m_chassis.resetPose(new Pose2d(0, 0, Rotation2d.fromDegrees(180)))),
+          new SetIntake(m_Dave_Intake, DoubleSolenoid.Value.kReverse, Constants.CUBE_HOLD_SPEED),
+          new SetArm(m_arm, Constants.ELBOW_MID_DIAG_TELEOP, Constants.SHOULDER_MID_DIAG_TELEOP),
+          new SetIntake(m_Dave_Intake, DoubleSolenoid.Value.kReverse, -0.2),
+          new WaitCommand(0.4), 
+          new SetIntake(m_Dave_Intake, DoubleSolenoid.Value.kReverse, Constants.CUBE_HOLD_SPEED),
+          new SetArmEnd(m_arm, Constants.ELBOW_STOW, Constants.SHOULDER_STOW),
+          new AutoChargeStation(m_chassis, Constants.CHARGE_SPEED),
+          new Park(m_chassis)
+        );
+      break; }
 
       case CONE_TAXI_EIGHT: {
       PathPlannerTrajectory eightHotel = PathFunctions.createTrajectory("8Hotel");
