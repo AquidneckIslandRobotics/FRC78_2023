a0e4ed267be2335d66278a84381835a178dc3d3b
AIR004 <AIR004>
2023-07-20 20:21:37 -0400

    Started Editing the swerve code for NEOs

--- src/main/java/frc/robot/classes/SwerveModule.java
+++ src/main/java/frc/robot/classes/SwerveModule.java
@@ -4,6 +4,9 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -23,8 +26,10 @@ public class SwerveModule {
     private Rotation2d angleOffset;
     private Rotation2d lastAngle;
 
-    private TalonFX mAngleMotor;
-    private TalonFX mDriveMotor;
+    private CANSparkMax mAngleMotor;
+    private CANSparkMax mDriveMotor;
+    private SparkMaxPIDController mAnglePidController;
+    private SparkMaxPIDController mDrivePidController;
     private CANCoder angleEncoder;
 
     public double CANcoderInitTime = 0.0;
@@ -43,13 +48,33 @@ public class SwerveModule {
 
         /* Angle Motor Config */
         // mAngleMotor = new TalonFX(moduleConstants.angleMotorID, "drivetrainCAN");
-        mAngleMotor = new TalonFX(moduleConstants.angleMotorID);
+        mAngleMotor = new CANSparkMax(moduleConstants.angleMotorID, MotorType.kBrushless);
         configAngleMotor();
 
         /* Drive Motor Config */
         // mDriveMotor = new TalonFX(moduleConstants.driveMotorID, "drivetrainCAN");
-        mDriveMotor = new TalonFX(moduleConstants.driveMotorID);
+        mDriveMotor = new CANSparkMax(moduleConstants.driveMotorID, MotorType.kBrushless);
         configDriveMotor();
+
+        mAnglePidController = mAngleMotor.getPIDController();
+        
+        mDrivePidController = mDriveMotor.getPIDController();
+
+        mAnglePidController.setP(Constants.Swerve.ANGLE_KP);
+        mAnglePidController.setI(Constants.Swerve.ANGLE_KI);
+        mAnglePidController.setD(Constants.Swerve.ANGLE_KD);
+        mAnglePidController.setIZone(0);
+        mAnglePidController.setFF(Constants.Swerve.ANGLE_KF);
+        mAnglePidController.setOutputRange(-1, 1);
+
+        mDrivePidController.setP(Constants.Swerve.DRIVE_KP);
+        mDrivePidController.setI(Constants.Swerve.DRIVE_KI);
+        mDrivePidController.setD(Constants.Swerve.DRIVE_KD);
+        mDrivePidController.setIZone(0);
+        mDrivePidController.setFF(Constants.Swerve.DRIVE_KF);
+        mDrivePidController.setOutputRange(-1, 1);
+
+    
 
         lastAngle = getState().angle;
     }
@@ -73,12 +98,13 @@ public class SwerveModule {
     private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
         if (isOpenLoop) {
             double percentOutput = desiredState.speedMetersPerSecond / Constants.Swerve.MAX_SPEED;
-            mDriveMotor.set(ControlMode.PercentOutput, percentOutput);
+            mDriveMotor.set(percentOutput);
         } else {
-            double velocity = Calculations.MPSToFalcon(desiredState.speedMetersPerSecond,
+            double velocity = Calculations.MPSToFalcon(desiredState.speedMetersPerSecond,//initial falcon velocity and behind the scene math. Takes in desired speed with wheel an GR
                     Constants.Swerve.WHEEL_CIRCUMFERENCE, Constants.Swerve.DRIVE_GEAR_RATIO);
-            mDriveMotor.set(ControlMode.Velocity, velocity, DemandType.ArbitraryFeedForward,
-                    feedforward.calculate(desiredState.speedMetersPerSecond));
+            double setPoint = velocity;                         //Sets the variable "set point" to the velocity above, allowing NEO to access this velocity
+            //Uses the pid controller through the neos to basically control the velocity as falcon
+            mDrivePidController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
         }
     }
 
@@ -92,8 +118,8 @@ public class SwerveModule {
             angle = desiredState.angle;
         }
 
-        mAngleMotor.set(ControlMode.Position,
-                Calculations.degreesToFalcon(angle.getDegrees(), Constants.Swerve.ANGLE_GEAR_RATIO));
+        //mAngleMotor.set(Calculations.degreesToFalcon(angle.getDegrees(), Constants.Swerve.ANGLE_GEAR_RATIO));
+        mAnglePidController.setReference(angle.getDegrees(), CANSparkMax.ControlType.kPosition);
         lastAngle = angle;
     }
 
